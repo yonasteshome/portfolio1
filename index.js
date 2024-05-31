d4d32ad3a4e4597926f15f988895418eed1c6b29
Yonas Teshome <140391850+yonasteshome@users.noreply.github.com>
2024-05-31 11:07:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var color=document.getElementById("button").innerHTML;
+color=color.toString()
+// change the background color of the page
+function change(){
+    document.getElementById("button").addEventListener("click",function(){
+        if(color=="dark"){
+            document.getElementById("nav-color").style.backgroundColor="white";
+            document.getElementById("nav-color").style.color="black"
+            color="white"
+            document.body.style.background="white";
+            document.getElementById("container-fluid").style.color="black"
+            document.getElementById("p").style.color="black"
+            document.getElementById("n").style.color="black"
+            document.getElementById("a").style.color="black"
+            document.getElementById("s").style.color="black"
+            
+
+        }
+        else if(color=="white"){
+            document.getElementById("nav-color").style.backgroundColor="black";
+            document.getElementById("nav-color").style.color="white"
+            color="black"
+            document.body.style.background="black";
+            document.getElementById("container-fluid").style.color="white"
+            document.getElementById("p").style.color="white"
+            document.getElementById("n").style.color="white"
+            document.getElementById("a").style.color="white"
+            document.getElementById("s").style.color="white"
+
+        }
+    })
+    }
+change();
+var content=document.getElementById("para").textContent;
+var temp=document.getElementById("para");
+temp.textContent="";
+function result(isWriting,i,text){
+    if(isWriting){
+        if(content.length>i){
+            temp.textContent+=text.charAt(i);
+        setTimeout(() => result(true,i+1,content),100)
+        }
+        else{
+            setTimeout(() => result(false,content.length-1,content),100)
+        }
+    }
+    else{
+        if(i>=0){
+        temp.textContent=text.substring(0,i);
+        setTimeout(() => result(false,i-1,text),100)
+    }
+        else{
+            setTimeout(() => result(true,0,content),100)
+        }
+}
+    
+}
+result(true,0,content);
